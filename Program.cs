d8d1abf1d6c20dabce397c3364d4968ad5135717
Jack Hope <jfhope3000@gmail.com>
2019-09-25 09:18:41 -0500

    Commit changes to Lesson 6 - String & More

--- Program.cs
+++ Program.cs
@@ -10,15 +10,33 @@ namespace CSharpClass
         static void Main(string[] args)
         {   
             // Console.WriteLine("Hello World!");
-            MyLesson5Examples();
+            // MyLesson5Examples();
+            MyLesson6Examples();
         } // End Method Main
+
+        static void MyLesson6Examples()
+        {
+            Lesson6 mySix = new Lesson6();
+            mySix.MyTrimExample();
+            mySix.MyEqualsExample();
+            mySix.MyLowerUpperExample();
+            mySix.MyStringLength();
+            string myJoined = mySix.MyJoinedStrings("Happy", "Coding");
+            Console.WriteLine(myJoined);
+            string myFav = mySix.MyStringBuilder("spaghetti", 8);
+            Console.WriteLine(myFav);
+            mySix.MyExampleChar();
+            mySix.MyEscapeExample();
+            mySix.MyPlaceHolderExample("Black Tiger", "bomb");
+
+        } // end method my lesson 6 examples
 
         static void MyLesson5Examples()
         {
             Lesson5 myFive = new Lesson5();
             myFive.ConvertFloatToInt();
             myFive.LongFromInt();
-        } // End method my lessons 5 examples
+        } // end method my lessons 5 examples
 
-    } // End class
-} // End namespace    
+    } // end class
+} // end namespace    
